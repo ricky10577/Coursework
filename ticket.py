f84f4b4546c34d897ae91ebc8e5b909417bc2bac
Ricardo <ricardocassim@live.com>
2024-12-13 21:19:33 +0000

    version two - ticket function

--- ticket.py
+++ ticket.py
@@ -9,3 +9,19 @@ def quantity_prompt():
             print("Invalid input. Please enter a whole positive number.")
         except ValueError:
             print("Invalid input. Please enter a number.")
+
+def ticket_purchase():
+    locations = {
+            '1': ("London", 10.15),
+            '2': ("Birmingham", 21.40),
+            '3': ("Manchester", 22.50)
+        }
+    
+    while True:
+        print("\nWhere are you travelling to?\n ")
+        for key, (location, price) in locations.items():
+            print(f"{key} - {location} (£{price:.2f})")
+        choice = input("\nPlease enter your choice (1-3): ")
+        if choice in locations:
+            return choice, locations[choice]
+        print("Invalid choice. Please try again.")
